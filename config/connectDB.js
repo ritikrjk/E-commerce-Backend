fe8a4fb11c29f523a483cd4fb0c92ed787d6870c
ritikrjk <ritikjain.ja@gmail.com>
2025-02-07 23:18:36 +0530

    removed uri

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,9 +2,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGO_DB_URI =
-  "mongodb+srv://newaddress445:gIvJCXcpQUDN8196@cluster0.sawpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
 if (!process.env.MONGODB_URI) {
   throw new Error("Please provide mongodb uri in .env file");
 }
